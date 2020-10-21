d4ad914ba48438a628a144f8cc9617df6f0b05c7
ilvytex <octavi.rodriguez@gmail.com>
2020-10-21 16:09:50 +0200

    refactor and css

--- js/finalize.js
+++
@@ -1,8 +0,0 @@
-function end() {
-  clearInterval(chrono);
-  if (currentLevel < levels.length - 1) {
-    document.querySelector("#levelUp").classList.add("visible");
-  } else {
-    document.querySelector("#endGame").classList.add("visible");
-  }
-}
--- js/gameover.js
+++
@@ -1,8 +0,0 @@
-function gameOver() {
-  clearInterval(chrono);
-  document.querySelector("#gameOver").classList.add("visible");
-}
-
-function timeUp() {
-  document.querySelector("#timeUp").classList.add("visible");
-}
